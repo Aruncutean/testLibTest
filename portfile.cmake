e1d79f88d383e768ff3ca0923f8dbd5477c9dd25
Aruncutean <52048476+Aruncutean@users.noreply.github.com>
2025-01-05 21:41:52 +0200

    add

--- portfile.cmake
+++ portfile.cmake
@@ -1,12 +1,16 @@
+set(VCPKG_ROOT "C:/vcpkg")
+
+include(${VCPKG_ROOT}/scripts/cmake/vcpkg.cmake)
+
 set(MYLIBRARY_VERSION "1.0.0")
-set(MYLIBRARY_URL "https://github.com/Aruncutean/testLibTest/releases/download/v1.0.0/testlib.zip")
+set(MYLIBRARY_URL "https://github.com/Aruncutean/testLibTest/releases/download/v1.0.1/testLib.zip")
 set(MYLIBRARY_SHA512 "02A46E8715CFA2460597B32BF948706B7CDCAFC2A925DF53C57AAEF6217D88AA1085D376F6F70AC5FF746135ABF95262ADAC3B7B39027480C4688CDC079133DD")
-set(DOWNLOAD_PATH ${DOWNLOADS}/testlib.zip)
+set(DOWNLOAD_PATH ${DOWNLOADS}/testlib-${MYLIBRARY_VERSION}.zip)
 # Download the archive
 vcpkg_download_distfile(
         OUT_SOURCE_PATH ${SOURCE_PATH}
         URLS  ${MYLIBRARY_URL}
-        FILENAME "testlib.zip"
+        FILENAME "testlib-${MYLIBRARY_VERSION}.zip"
         SHA512 ${MYLIBRARY_SHA512}
 )
 
